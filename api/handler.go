b08734f37937a60da122cd53ec5ccf63f69ee388
Jopiol <you@example.com>
2024-11-16 23:48:28 +0400

    adding POST req & fixing problems with GET req

--- api/handler.go
+++ api/handler.go
@@ -1,11 +1,42 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jexlor/cs2api/db"
 )
 
 func LandingPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
+}
+func GetAllSkins(c *gin.Context) {
+	skins, err := getAllSkinsJson()
+	if err != nil {
+		log.Printf("Error fetching skins: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Couldn't serve skins!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, skins)
+}
+
+func AddSkin(c *gin.Context) {
+	var skin Skin
+
+	if err := c.ShouldBindJSON(&skin); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Couldn't parse json!"})
+		return
+	}
+
+	_, err := db.DB.Exec(`INSERT INTO skins(name, rarity, collection, quality, price, url)
+	VALUES (?, ?, ?, ?, ?, ?)`, skin.Name, skin.Rarity, skin.Collection, skin.Quality, skin.Price, skin.Url)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Couldn't add skin!"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Skin added!"})
 }
--- api/json.go
+++ api/json.go
@@ -1,1 +1,41 @@
 package api
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jexlor/cs2api/db"
+)
+
+// structure of skins
+type Skin struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Rarity     string `json:"rarity"`
+	Collection string `json:"collection"`
+	Quality    string `json:"quality"`
+	Price      string `json:"price"` //type string in order to handle currency symbols
+	Url        string `json:"url"`
+}
+
+func getAllSkinsJson() ([]Skin, error) {
+	rows, err := db.DB.Query("SELECT * FROM skins")
+	if err != nil {
+		log.Printf("Error executing query: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var skinsList []Skin
+
+	for rows.Next() {
+		var s Skin
+		if err := rows.Scan(&s.Id, &s.Name, &s.Rarity, &s.Collection, &s.Quality, &s.Price, &s.Url); err != nil {
+			log.Printf("Error scanning row: %v", err)
+			return nil, err
+		}
+		skinsList = append(skinsList, s)
+	}
+	fmt.Println(len(skinsList))
+	return skinsList, nil
+}
