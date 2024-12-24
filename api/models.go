c95cd6402ec598943402304e2e710770c9f889c1
jexlor <giorgitenoshvili4@gmail.com>
2024-12-24 22:44:15 +0400

    Update models

--- api/models.go
+++ api/models.go
@@ -2,13 +2,14 @@ package api
 
 // structure of skins
 type Skin struct {
-	Id         int    `json:"id"`
-	Name       string `json:"name"`
-	Rarity     string `json:"rarity"`
-	Collection string `json:"collection"`
-	Quality    string `json:"quality"`
-	Price      string `json:"price"` //type string in order to handle currency symbols
-	Url        string `json:"url"`
+	Id             int    `json:"id"`
+	Name           string `json:"name"`
+	Rarity         string `json:"rarity"`
+	Collection     string `json:"collection"`
+	Quality        string `json:"quality"`
+	Price          string `json:"price"`           //type string in order to handle currency symbols
+	StattrackPrice string `json:"stattrack_price"` //type string in order to handle currency symbols
+	Url            string `json:"url"`
 }
 
 type Col struct {
